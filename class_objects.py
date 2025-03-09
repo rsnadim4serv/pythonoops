573da488c7e1146309f732db25da104662c0ddc8
rsnadim4serv <rsnadim.pro5133@gmail.com>
2025-03-09 22:03:21 +0600

    class_objects file changes

--- class_objects.py
+++ class_objects.py
@@ -2,13 +2,29 @@ class myStore:
     def __init__(self,storeName:str):
         self.storeName=storeName
 
-    def Vegetables(self):
-        print("Working on progess!")
+    def Vegetables(self)->None:
+        vdic={"tomato":50,"carrot":40,"cucumber":25,"radish":30}
+        print("Here, we have..\nNames\t\tPrice(per kg)\n")
+        for k,v in vdic.items():
+            print(f"{k}\t\t{v}")
+        vname=input("Enter your vegetable: ")
+        flag=0
+        for prod in vdic.keys():
+            if(vname==prod):
+                flag=1
+                print(f"You have to pay {vdic[prod]}/=")
+                pay=int(input("Enter your payment: "))
+                if(pay>=vdic[prod]):
+                    print(f"You have purchased {prod}, here your change {pay-vdic[prod]}/=")
+                else:
+                    print("Inadequate balance/money!")
+        if(flag==0):
+            print("doesn't exit in out products")
 
-    def DryFruits(self):
+    def DryFruits(self)->None:
         print("DryFruits-Under development!")
 
-    def Carbohydrate(self):
+    def Carbohydrate(self)->None:
         print("Working on Carbohydrate!")
 
 store1 = myStore("MasterStore")
